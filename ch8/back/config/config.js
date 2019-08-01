5d69a37ba03ec2ff37b4c8fa2072a232f52ed956
weonsj <weonsj@gmail.com>
2019-08-01 14:33:57 +0900

    update : config.js

--- ch8/back/config/config.js
+++ ch8/back/config/config.js
@@ -6,22 +6,22 @@ module.exports = {
   development: {
     username: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'react-nodebird',
-    host: '127.0.0.1',
+    database: 'react_nodebird',
+    host: '3.130.41.181',
     dialect: 'mysql',
   },
   test: {
     username: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'react-nodebird',
-    host: '127.0.0.1',
+    database: 'react_nodebird',
+    host: '3.130.41.181',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'react-nodebird',
-    host: '127.0.0.1',
+    database: 'react_nodebird',
+    host: '3.130.41.181',
     dialect: 'mysql',
   },
 };
