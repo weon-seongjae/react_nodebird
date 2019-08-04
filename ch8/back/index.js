60da3097f464bda5d23a2252879a1eb756233691
weonsj <weonsj@gmail.com>
2019-08-04 15:53:34 +0900

    update : back package.json option

--- ch8/back/index.js
+++ ch8/back/index.js
@@ -52,6 +52,10 @@ app.get('/', (req, res) => {
   res.send('react nodebird 백엔드 정상동작');
 });
 
+app.get('/', (req, res) => {
+  res.send('react nodebird 백엔드 정상동작');
+});
+
 // API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
