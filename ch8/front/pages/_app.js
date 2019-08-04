6a955fb8e4924b557529863b424fab358fbe5196
weonsj <weonsj@gmail.com>
2019-08-04 19:49:00 +0900

    update : _app.js

--- ch8/front/pages/_app.js
+++ ch8/front/pages/_app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withRedux from 'next-redux-wrapper';
-import withReduxSaga from 'next-redux-saga'; // 서버사이드 렌더링에 필수요소
+import withReduxSaga from 'next-redux-saga';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import axios from 'axios';
 import Helmet from 'react-helmet';
-import { Container } from 'next/app';
+import App, { Container } from 'next/app';
 
 import AppLayout from '../components/AppLayout';
 import reducer from '../reducers';
@@ -23,45 +23,48 @@ import { LOAD_USER_REQUEST } from '../reducers/user';
 //   }
 // }
 
-const NodeBird = ({ Component, store, pageProps }) => (
-  <Container>
-    <Provider store={store}>
-      <Helmet
-        title="NodeBird"
-        htmlAttributes={{ lang: 'ko' }}
-        meta={[{
-          charset: 'UTF-8',
-        }, {
-          name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=yes,viewport-fit=cover',
-        }, {
-          'http-equiv': 'X-UA-Compatible', content: 'IE=edge',
-        }, {
-          name: 'description', content: 'weonsj의 NodeBird SNS',
-        }, {
-          name: 'og:title', content: 'NodeBird',
-        }, {
-          name: 'og:description', content: 'weonsj의 SNS',
-        }, {
-          property: 'og:type', content: 'website',
-        }, {
-          property: 'og:image', content: 'http://keysns.com/favicon.ico',
-        }]}
-        link={[{
-          rel: 'shortcut icon', href: '/favicon.ico',
-        }, {
-          rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css',
-        }, {
-          rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css',
-        }, {
-          rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css',
-        }]}
-      />
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </Provider>
-  </Container>
-);
+const NodeBird = ({ Component, store, pageProps }) => {
+  return (
+    <Container>
+      <Provider store={store}>
+        <Helmet
+          title="NodeBird"
+          htmlAttributes={{ lang: 'ko' }}
+          meta={[{
+            charset: 'UTF-8',
+          }, {
+            name: 'viewport',
+            content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover',
+          }, {
+            'http-equiv': 'X-UA-Compatible', content: 'IE=edge',
+          }, {
+            name: 'description', content: '제로초의 NodeBird SNS',
+          }, {
+            name: 'og:title', content: 'NodeBird',
+          }, {
+            name: 'og:description', content: '제로초의 NodeBird SNS',
+          }, {
+            property: 'og:type', content: 'website',
+          }, {
+            property: 'og:image', content: 'https://nodebird.com/favicon.ico',
+          }]}
+          link={[{
+            rel: 'shortcut icon', href: '/favicon.ico',
+          }, {
+            rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css',
+          }, {
+            rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css',
+          }, {
+            rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css',
+          }]}
+        />
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </Provider>
+    </Container>
+  );
+};
 
 NodeBird.propTypes = {
   Component: PropTypes.elementType.isRequired,
@@ -69,17 +72,11 @@ NodeBird.propTypes = {
   pageProps: PropTypes.object.isRequired,
 };
 
-// const middleware = (store) => (next) => (action) => {
-//   next(action);
-// };
-//= =====middleware만드는 방법 : currying - 인자 하나를 받아 다른 함수를 리턴
-
-// ==================next에서 getInitialProps 실행. context도 같이 넣어줌. 이것에 각 Component에서 실행됨.===============
 NodeBird.getInitialProps = async (context) => {
   const { ctx, Component } = context;
   let pageProps = {};
-  const state = ctx.store.getState(); // user 정보 가져옴, user 정보도 서버사이드 렌더링 구현됨
-  const cookie = ctx.isServer ? ctx.req.headers.cookie : ''; // 쿠키가 위치하는 곳
+  const state = ctx.store.getState();
+  const cookie = ctx.isServer ? ctx.req.headers.cookie : '';
   axios.defaults.headers.Cookie = '';
   if (ctx.isServer && cookie) {
     axios.defaults.headers.Cookie = cookie;
@@ -90,26 +87,23 @@ NodeBird.getInitialProps = async (context) => {
     });
   }
   if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx) || {}; // hashtag.js에서 리턴한 값이 pageProps에 담김. pageProps는 위의 NodeBird의 pageProps로 넘김, index.js의 Home.getInitialProps = async (context)가 됨. 게시글 먼저 가져오는 경우
+    pageProps = await Component.getInitialProps(ctx) || {};
   }
-  return { pageProps }; // component의 props
+  return { pageProps };
 };
-// =================================================================================
 
 const configureStore = (initialState, options) => {
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware];
-  const enhancer = process.env.NODE_ENV === 'production' // 실제 서비스
+  const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
     : compose(
       applyMiddleware(...middlewares),
       !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
     );
   const store = createStore(reducer, initialState, enhancer);
-  store.sagaTask = sagaMiddleware.run(rootSaga); // 서버사이드 렌더링할 때 store.sagaTask를 변수로 설정. withReduxSaga가 내부에서 필요로 함
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
 export default withRedux(configureStore)(withReduxSaga(NodeBird));
-
-// 레이아웃은 _app.js에서 코딩을 한다.
